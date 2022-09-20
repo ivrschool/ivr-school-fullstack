34c3a33f648692c11e79fe79aea375b4d9631a24
ivrschool <chandravanshi.pankaj@gmail.com>
2022-09-20 14:33:55 +0530

    nabar+bannaer+service added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import TopBanner from './components/TopBanner/TopBanner';
+import TopNavigation from './components/TopNavigation/TopNavigation';
+import Services from './components/Services/Services';
+
 
 function App() {
   return (
     <div>
+      <TopNavigation />
+      <TopBanner />
+      <Services />
       
     </div>
   );
---
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -0,0 +1,62 @@
+import React, { Component, Fragment } from 'react'
+import { Navbar,Nav } from 'react-bootstrap'
+import whiteLogo from '../../asset/image/logo_white.png';
+import blackLogo from '../../asset/image/logo_black.png';
+
+class TopNavigation extends Component {
+
+    constructor(){
+          super();
+          this.state={
+              navBarTitle: "navTitle",
+              navBarLogo: [whiteLogo], // object
+              navBarBack: "navBackground",
+              navBarItem:"navItem"
+          }
+     }
+
+     onScroll=()=>{
+          if(window.scrollY>100){
+               this.setState({navBarTitle:'navTitleScroll',navBarLogo:[blackLogo],navBarBack:'navBackgroundScroll',navBarItem:'navItemScroll'})
+          } else if (window.scrollY < 100) {
+               this.setState({navBarTitle:'navTitle',navBarLogo:[whiteLogo],navBarBack:'navBackground',navBarItem:"navItem"})
+          }
+     }
+
+          componentDidMount(){
+               window.addEventListener('scroll',this.onScroll)
+          }
+    
+     render() {
+          return (
+              <Fragment>
+                  
+                  
+
+                    <Navbar className={this.state.navBarBack} collapseOnSelect fixed="top" expand="lg" variant="dark">
+
+                      {/* <Container> */}
+                      {/* <Navbar.Brand href="#home">ivr school</Navbar.Brand> */}
+                            <Navbar.Brand className={this.state.navBarTitle} href="#home"> <img src={this.state.navBarLogo} /> </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="mr-auto">
+                                    <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
+                                    <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
+                                    <Nav.Link className={this.state.navBarItem}  href="#deets">SERVICE</Nav.Link>
+                                    <Nav.Link className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
+                                    <Nav.Link className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
+                                    <Nav.Link className={this.state.navBarItem} href="#deets">CONTACT US</Nav.Link>
+
+
+                                </Nav>
+                            </Navbar.Collapse>
+                            {/* </Container> */}
+                        </Navbar>
+
+            </Fragment>
+          )
+     }
+}
+
+export default TopNavigation
